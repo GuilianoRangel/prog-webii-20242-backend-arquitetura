19698d0e95d75bf8734554e730cf9812d19a0dcd
Guiliano Rangel <guiliano@gmail.com>
2024-09-10 19:10:06 -0300

    [progwebii-04] Exemplos de teste unitário

---
+++ src/test/java/br/ueg/progweb2/arquitetura/ArqApplicationTests.java
@@ -0,0 +1,11 @@
+package br.ueg.progweb2.arquitetura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ArqApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
